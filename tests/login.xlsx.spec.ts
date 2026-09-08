135220e2eff0fc8eac2efd508c4990defa70ed2a
Chu Nguyen Quang Thai <iamtk4i@gmail.com>
2026-09-08 22:57:41 +0700

    await step

--- tests/login.xlsx.spec.ts
+++ tests/login.xlsx.spec.ts
@@ -7,19 +7,25 @@ test.describe("Login Driven Test - XLSX", () => {
     for (const data of loginUser as any[]) {
         test(`Login Test for ${data.username}`, async ({page}) =>{
             test.skip(data.run !== 'true', `Skipping test for ${data.username} as run is set to false`);
-            
-            const loginPage = new LoginPage(page);
-            await loginPage.gotoLoginPage();
-            await loginPage.login(data.username, data.password);
 
-            if (data.expected == 'success' ){
-                await expect(page).toHaveTitle(/Swag Labs/);
-                await expect(page).toHaveURL(/inventory.html/);
-            } else {
-                await expect(page).toHaveURL('https://www.saucedemo.com/');
-                await expect(loginPage.errorMessage).toBeVisible();
-            }
-            
+            const loginPage = new LoginPage(page);
+            await test.step('Go to Login Page', async () =>{
+                await loginPage.gotoLoginPage();
+            });
+
+            await test.step('Login with ' + data.username + ' and ' + data.password, async () =>{
+                await loginPage.login(data.username, data.password);
+            });
+
+            await test.step('Verify Login', async () =>{
+                if (data.expected == 'success' ){
+                    await expect(page).toHaveTitle(/Swag Labs/);
+                    await expect(page).toHaveURL(/inventory.html/);
+                } else {
+                    await expect(page).toHaveURL('https://www.saucedemo.com/');
+                    await expect(loginPage.errorMessage).toBeVisible();
+                }
+            });
         })
     }
 })
